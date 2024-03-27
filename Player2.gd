8b8c8feb87198074b355a1d7dbc3e0ede127e21b
wareya <wareya@gmail.com>
2024-03-27 11:14:13 -0400

    make more cramped

--- Player2.gd
+++ Player2.gd
@@ -8,11 +8,11 @@ func _ready():
     for i in range(10000):
         var node := preload("res://enemy.tscn").instantiate()
         scene.add_child(node)
-        node.global_position.x = randf_range(-10000.0, 10000.0)
-        node.global_position.y = randf_range(-5000.0, 5000.0)
+        node.global_position.x = randf_range(-1000.0, 1000.0)
+        node.global_position.y = randf_range(-500.0, 500.0)
 
 func _process(delta: float) -> void:
-    $Label.text = "Area2D check\n10000 entities, 20k x 10k spawn area\n" + str(Engine.get_frames_per_second()) + "fps"
+    $Label.text = "Area2D check\n10000 entities, 2k x 1k spawn area\n" + str(Engine.get_frames_per_second()) + "fps"
     var checks := 0
     for node in get_overlapping_areas():
         node.do_in_range_logic()
--- Player2_distcheck.gd
+++ Player2_distcheck.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 var enemies : Array[Node] = []
 
@@ -10,15 +10,15 @@ func _ready():
     for i in range(10000):
         var node := preload("res://enemyNonArea2D.tscn").instantiate()
         scene.add_child(node)
-        node.global_position.x = randf_range(-10000.0, 10000.0)
-        node.global_position.y = randf_range(-5000.0, 5000.0)
+        node.global_position.x = randf_range(-1000.0, 1000.0)
+        node.global_position.y = randf_range(-500.0, 500.0)
         enemies.push_back(node)
 
 func _process(delta: float) -> void:
-    $Label.text = "Distance check\n10000 entities, 20k x 10k spawn area\n" + str(Engine.get_frames_per_second()) + "fps"
+    $Label.text = "Distance check\n10000 entities, 2k x 1k spawn area\n" + str(Engine.get_frames_per_second()) + "fps"
     var checks := 0
     var global_pos := global_position
-    var dist := 410.0
+    var dist := 400.01
     var dist_sq := dist * dist
     for node in enemies:
         if global_pos.distance_squared_to(node.global_position) < dist_sq:
